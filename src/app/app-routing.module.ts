5c3aa7b6a545f4d469cd47e4a3658d4d03d7bcc2
trainandhire <projects.trainandhire@gmail.com>
2022-12-09 09:49:19 +0530

    calculator completed.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
@@ -10,7 +11,8 @@ const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'dashboard', component: DashboardComponent, children:[
     {path:'home', component: HomeComponent},
-    {path:'calculator', component: CalculatorComponent}
+    {path:'calculator', component: CalculatorComponent},
+    {path:'data-binding', component: DataBindingComponent}
   ]},
   {path:'',component:LoginComponent},
   {path:'**',component:PagenotfoundComponent}
